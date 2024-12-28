505911f1cdf83964e4fb9ee0cd3b49cdb1ffe009
Yab <yeabalex18@gmail.com>
2024-12-28 01:09:54 -0800

    apiv1

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const userURL =
-  process.env.NODE_ENV === "development" ? "http://35.162.119.64" : "http://35.162.119.64";
+  process.env.NODE_ENV === "development" ? "http://35.162.119.64/api/v1" : "http://35.162.119.64/api/v1";
 
 export const clientURL =
   process.env.NODE_ENV === "development" ? "https://curatefy.vercel.app/" : "https://curatefy.vercel.app/";
