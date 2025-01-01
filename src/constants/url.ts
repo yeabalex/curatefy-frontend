ba972fcebc4f99b7b0a645f62f696341f74ea759
Yab <yeabalex18@gmail.com>
2025-01-01 04:31:32 -0800

    ors

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -5,5 +5,4 @@ dotenv.config();
 export const userURL =
   process.env.NODE_ENV === "development" ? "https://curatefy-backend-production.up.railway.app/api/v1" : "https://curatefy-backend-production.up.railway.app/api/v1";
 
-export const clientURL =
-  process.env.NODE_ENV === "development" ? "https://curatefy.vercel.app/" : "https://curatefy.vercel.app/";
+export const clientURL = "https://curatefy.vercel.app"
